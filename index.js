c8f1dd46fa1552b755500829ee6a3139825b21f9
john7319 <jonniekaras@gmail.com>
2024-03-18 17:11:47 +0300

    Completed adding mathematical functions

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1*num2;
+
+let random;
+random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 40;
+let num4 = 6;
+
+let mod;
+mod = num3%num4
+
+const max = (5,18,19,10,15,20)
+Math.max(max)
